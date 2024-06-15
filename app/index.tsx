f516d4cede0c9d7080d578b61a02619aa41658b7
ChocolateTunder <xothicycle@gmail.com>
2024-06-15 22:56:23 +1000

    Reformatted for index to be camera screen, added device ID input and submission button. TODO: Add functionality

--- app/index.tsx
+++ app/index.tsx
@@ -1,41 +1,42 @@
-import { View, Text, StyleSheet } from "react-native";
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-import * as SecureStore from 'expo-secure-store';
-
-const token_url = "https://api.snapi.com.au/gateway/oauth/oauth/token";
+import React from "react";
+import { Button, Text, View, TextInput } from "react-native";
+import CameraPage from "./camera";
 
 export default function Index() {
-    const [token, setToken] = useState(" ");
-    //setToken("Test");
+    const [deviceID, onChangeDeviceID] = React.useState('');
 
-    /*
-    Refresh token is stored in token, check for cached version using SecureStore and set token to anything found using SecureStore
-    If that token is accepted the .then function should lead user to home page
-    If the token is not accepted the .catch takes the user to the login page
-    */
-    useEffect(() => {
-        let payload = {
-            refresh_token: token,
-            grant_type: "refresh_token",
-        }
-        let axiosConfig = {
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
-            }
-        };
-        axios.post(token_url, payload, axiosConfig)
-        .then(function(response){
-            console.log(response)
-        })
-        .catch(function (error){
-            console.error("Error in verifying token: ", error);
-        });
-    }, []);
+    function submitCameraID(){
+        console.log("Submit!");
+    }
 
-    <View>
-        <Text>
-            Test
-        </Text>
-    </View>
+    
+
+    return (
+        <View
+        style={{
+            flex: 1,
+            height: 100,
+            padding: 20,
+            justifyContent: "center",
+            alignItems: "center",
+        }}
+        >   
+            <View style={{
+                flex: 0.6
+            }}>
+                <CameraPage/>
+            </View>
+
+            <TextInput
+                onChangeText={onChangeDeviceID}
+                value={deviceID}
+                placeholder="Device ID"
+            />
+            <Button
+                onPress={submitCameraID}
+                title="Submit Device ID"
+                color="green"
+            />
+        </View>
+  );
 }
