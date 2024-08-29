dba9962ec71951cd337a31cbc34f3b056fb082a5
ChocolateTunder <xothicycle@gmail.com>
2024-08-29 11:05:28 +1000

    Pre-box code checkpoint

--- API/api.tsx
+++ API/api.tsx
@@ -1,23 +1,13 @@
 import axios from 'axios';
 
 const API_URL = 'https://api.snapi.com.au/gateway';
-// process.env.EXPO_PUBLIC_TEMP_USERNAME
-// process.env.EXPO_PUBLIC_TEMP_PASSWORD
+const SNAPI_SERVER ='http://20.53.98.203';
+
 let axiosConfig = {
     headers: {
         'Content-Type': 'application/x-www-form-urlencoded'
     }
 };
-// import qs from 'qs';
-// const data = { 'bar': 123 };
-// const options = {
-//   method: 'POST',
-//   headers: { 'content-type': 'application/x-www-form-urlencoded' },
-//   data: qs.stringify(data),
-//   url,
-// };
-// axios(options);
-
 
 export async function login(){
     const LOGIN_URL = API_URL + '/oauth/oauth/token';
@@ -67,12 +57,12 @@ export async function deviceDetails(token:string, device:number){
     const URL = API_URL + '/manage/v2/device/deviceName';
 
     try {
-        const response = await axios.post(URL, {
-            deviceName: device,
-            access_token: token
-        }, axiosConfig)
-
-        return response.data;
+        const response = await axios.get(URL, {params:
+            {deviceName: device,
+            access_token: token}
+        })
+        console.log("API - Device details accessed", response.data)
+        return response.data.data;
     } catch(error) {
         console.error("Error in getting device details: ", error);
         throw error;
@@ -83,53 +73,36 @@ export async function deviceDetails(token:string, device:number){
 export async function getPicture(token:string, device:number, product:string){
     const IMAGE_URL = API_URL + '/manage/picture/lastedPicture';
     try {
-        const response = await axios.post(IMAGE_URL, {
-            currentPage: 1,
+        const response = await axios.get(IMAGE_URL, {params:
+            {currentPage: 1,
             pageSize: 1,
             deviceName: device,
-            picMode: 1,
+            picMode: 2,
             productKey: product,
-            access_token: token
-        }, axiosConfig)
-        return response.data.list;
+            access_token: token}
+        })
+        console.log("API - Picture info gotten");
+        return response.data.data.list[0].path;
     } catch(error){
         console.error("Error in getting device picture: ", error);
         throw error;
     }
 }
+export async function uploadCutoutPic(img64:string, access_token:string) {
+    const URL = SNAPI_SERVER + '/gateway/manage/picture/uploadCutPic';
+    const data = new FormData();
+    data.append('pic', new Blob([img64], { type: 'image/jpg' }));
+    data.append('access_token', access_token);
+    
+    try {
+        axios.post(URL, data).then(function(response){
+            console.log("API - Upload Successful", response)
+            return response.data.data;
+        });
+    } catch (error) {
+        console.error("Error in getting device picture: ", error);
+        throw error;
+    }
 
-// export function getPicture(token:string, device:number, product:string){
-//     const IMAGE_URL = API_URL + '/manage/picture/lastedPicture';
-
-//     axios.post(IMAGE_URL, {
-//         currentPage: 1,
-//         pageSize: 1,
-//         deviceName: device,
-//         picMode: 1,
-//         productKey: product,
-//         access_token: token
-//     }, axiosConfig)
-//     .then(function(response){
-//         console.log(response)
-//         return response.data.list
-//     })
-//     .catch(function(error){
-//         console.error("Error in getting device picture: ", error);
-//     })
-// }
-
-// export function deviceDetails(token:string, device:number){
-//     const URL = API_URL + '/manage/v2/device/deviceName';
-
-//     axios.post(URL, {
-//         deviceName: device,
-//         access_token: token
-//     }, axiosConfig)
-//     .then(function(response){
-//         console.log(response)
-//         return response.data
-//     })
-//     .catch(function(error){
-//         console.error("Error in getting device details: ", error);
-//     })
-// }
+    return 'API Error';
+}
--- app/(tabs)/calibrate.tsx
+++ app/(tabs)/calibrate.tsx
@@ -1,24 +1,20 @@
 import { useEffect, useState } from "react";
 import { View, Text, StyleSheet, TextInput, Pressable, Image, Button } from "react-native";
 import { useAppContext } from '../context';
-import { deviceDetails, getPicture } from "@/API/api";
+import { deviceDetails, getPicture, uploadCutoutPic } from "@/API/api";
 import { red } from "react-native-reanimated/lib/typescript/reanimated2/Colors";
 
 export default function calibrate() {
     const [decimals, onChangeDecimals] = useState('');
     const [digits, onChangeDigits] = useState('');
     const [meterID, onChangeMeterID] = useState(1800);
-    const [angle, onAngleChange] = useState(0);
+    const [angle, onAngleChange] = useState('');
     const [result, onResultChange] = useState(0);
     const [imgPath, updateImagePath] = useState('');
+    const [tempImgPath, updateTempImgPath] = useState('');
     const [imgLoaded, onImgLoad] = useState(false);
     const { authToken, setAuthToken, refreshToken, setRefreshToken, qrCode, setQRCode } = useAppContext();
-    
-    function loadImage(){
 
-    }
-  
-  useEffect(() =>{
     async function getProductKey(){
       try {
         const details = await deviceDetails(authToken, qrCode);
@@ -31,35 +27,72 @@ export default function calibrate() {
     async function getDeviceConfigImage() {
       try {
         const product = await getProductKey();
-        const imgList = await getPicture(authToken, qrCode, product);
-        console.log(imgList);
+        console.log("Product key: ", product);
+        const path = await getPicture(authToken, qrCode, product);
+        updateImagePath(path);
       } catch(error){
         console.log("Error in getting device image")
       }
     }
-    
-    getDeviceConfigImage;
-    
-  }, [])
-  // URL of the image to show after button press
-  const imageUrl = 'https://example.com/your-image.jpg';
+
+    function convertToBase64(url:string){
+      fetch(url).then(response => response.blob()).then(blob => {
+        const fileReader = new FileReader();
+        fileReader.readAsDataURL(blob);
+        return new Promise ((response) => {
+          fileReader.onloadend = () => {
+            response(fileReader.result?.toString); 
+        }})
+      })
+      return 'Calibration Error';
+    }
+
+    async function createCutout(imagePath:string, token:string){
+      try {
+        const img64:string = convertToBase64(imagePath);
+        const path = await uploadCutoutPic(img64, token);
+        updateTempImgPath(path);
+        console.log("Temp img path: ", tempImgPath);
+      } catch (error) {
+        
+      }      
+    }
+
+    function loadImage(){
+      onImgLoad(true)
+    }
+
+    function identify(){
+      createCutout(imgPath, authToken);
+    }
+  
+    useEffect(() =>{
+      getDeviceConfigImage();    
+    }, [])
 
   return (
     <View style={styles.container}>
       {imgLoaded ? (
         <Image
-          source={{ uri: imageUrl }}
+          source={{ uri: "http://20.53.98.203" + imgPath }}
           style={styles.image}
           resizeMode="contain"
         />
       ) : (
-        <Text style={styles.placeholder}>Press the button to load the image</Text>
+        <Text style={styles.placeholder}>Press the button to get the latest image from SNAPI device</Text>
       )}
 
-      <Button
-        title="Press me"
-        onPress={() => onImgLoad(true)}
-      />
+      {imgLoaded ? (
+        <View>
+          <Text>Angle:                    </Text><TextInput keyboardType="numeric" value={angle} onChangeText={newAngle => onAngleChange(newAngle)}/>
+          <Text>Number of digits:         </Text><TextInput keyboardType="numeric" value={digits} onChangeText={newDigits => onChangeDigits(newDigits)}/>
+          <Text>Number of decimal places: </Text><TextInput keyboardType="numeric" value={decimals} onChangeText={newDecimal => onChangeDecimals(newDecimal)}/>
+          <Text>Recognition result: {result}</Text>
+          <Button title="Identify" onPress={identify}/>
+        </View>
+      ) : (
+        <Button title="Load Config" onPress={loadImage}/>
+      )}
     </View>
   );
 }
@@ -79,4 +112,8 @@ const styles = StyleSheet.create({
     height: 200,
     marginBottom: 20,
   },
+  input: {
+    borderColor: "black",
+    borderWidth: 2
+  }
 });
